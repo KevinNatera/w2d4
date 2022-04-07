9583bb757e0861ac5dcf00243d9c8b90422326f9
Kevin <NateraKevin1@gmail.com>
2022-04-07 16:54:18 -0400

    completed recap project

--- recap_exercise_1/lib/problems.rb
+++ recap_exercise_1/lib/problems.rb
@@ -5,8 +5,19 @@
 # Example:
 #
 # all_vowel_pairs(["goat", "action", "tear", "impromptu", "tired", "europe"])   # => ["action europe", "tear impromptu"]
-def all_vowel_pairs(words)
+    def all_vowel_pairs(words)
+        vowels = ["a","e","i","o","u"]
+        pairs = []
 
+            words.each_with_index do |word1,idx1|
+                words.each_with_index do |word2,idx2|
+                    pair = "#{word1} #{word2}"
+                    if idx2 > idx1
+                        pairs << pair if vowels.all? { |vowel| pair.include?(vowel)}
+                    end
+                end
+            end
+       pairs
 end
 
 
@@ -17,8 +28,14 @@ end
 #
 # composite?(9)     # => true
 # composite?(13)    # => false
-def composite?(num)
 
+
+def composite?(num)
+    counter = 0
+    (2...num).each do |factor|
+        counter += 1 if num % factor == 0
+    end
+    counter > 0
 end
 
 
@@ -31,8 +48,18 @@ end
 #
 # find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
 # find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
-def find_bigrams(str, bigrams)
 
+def find_bigrams(str, bigrams)
+    arr = []
+
+    bigrams.each do |bigram|
+
+        str.each_char.with_index do |char,i|
+            arr << bigram if str[i - 1] + char == bigram
+        end
+    end
+
+    arr
 end
 
 class Hash
@@ -49,8 +76,18 @@ class Hash
     # hash_2 = {4=>4, 10=>11, 12=>3, 5=>6, 7=>8}
     # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
     # hash_2.my_select                            # => {4=>4}
+    
     def my_select(&prc)
-
+        hash = Hash.new()
+        
+            self.each do |k,v|
+                if prc != nil
+                    hash[k] = v if prc.call(k,v)
+                else 
+                   hash[k] = v if k == v
+                end
+            end
+        hash
     end
 end
 
@@ -64,7 +101,27 @@ class String
     # "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
     # "cats".substrings(2)  # => ["ca", "at", "ts"]
     def substrings(length = nil)
-
+        arr = []
+        str = ""
+       
+        self.each_char.with_index do |char1, idx1|
+            arr << char1 
+            str += char1
+            self.each_char.with_index do |char2, idx2|
+                if idx2 > idx1
+                    str += char2
+                    arr << str
+                end
+            end
+            str = ""
+        end
+        arr.select do |substring|
+            if length != nil
+                substring.length == length
+            else
+                substring
+            end
+        end
     end
 
 
@@ -78,6 +135,17 @@ class String
     # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
     # "zebra".caesar_cipher(4)    #=> "difve"
     def caesar_cipher(num)
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+            new_str = ""
 
+        self.each_char do |char|
+            old_idx = alphabet.index(char)
+            new_idx = old_idx + num
+            new_char = alphabet[new_idx % 26] 
+            new_str += new_char
+        end
+
+        return new_str
     end
+
 end
